917d3a6d927ec40b6d577d815523c4f3ef367ba9
TriciaBAA <pat.aspillaga@gmail.com>
2026-09-06 02:05:54 +0800

    Add hero-copy hidden behavior

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 const navbar = document.querySelector('.navbar');
+const heroCopy = document.querySelector('.hero-copy');
 let lastScrollY = window.scrollY;
 
-if (navbar) {
+if (navbar || heroCopy) {
     window.addEventListener('scroll', () => {
         const currentScrollY = window.scrollY;
 
-        if (currentScrollY <= 10 || currentScrollY < lastScrollY) {
-            navbar.classList.remove('navbar-hidden');
-        } else {
-            navbar.classList.add('navbar-hidden');
+        if (navbar) {
+            if (currentScrollY <= 10 || currentScrollY < lastScrollY) {
+                navbar.classList.remove('navbar-hidden');
+            } else {
+                navbar.classList.add('navbar-hidden');
+            }
+        }
+
+        if (heroCopy && currentScrollY > 80) {
+            heroCopy.classList.add('hero-copy-visible');
+        } else if (heroCopy) {
+            heroCopy.classList.remove('hero-copy-visible');
         }
 
         lastScrollY = currentScrollY;
